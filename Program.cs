7480e71ae03e8afc41fdeb0e36d6c2d5fa032224
Mathias Niklas Larsen <mnla@itu.dk>
2024-08-28 15:33:00 +0200

    Completed read function missing time.
    added the ability to read the messages from the csv file
    And further started on the cheep function
    
    Co-authored-by: Marcus <loef@itu.dk>

--- Program.cs
+++ Program.cs
@@ -6,17 +6,27 @@ if (args[0] == "read")
         using (StreamReader sr = new StreamReader("chirp_cli_db.csv"))
         {
             string line;
-
+            sr.ReadLine();
             while ((line = sr.ReadLine()) != null)
             {
-                Console.WriteLine(line);
-                Console.WriteLine("break");
+                string[] status = line.Split('"');
+                status[0] = status[0].Trim(',');
+                status[2] = status[2].Trim(',');
+                Console.WriteLine(status[0] + " @ " + status[2] + " " + status[1]);
             }
         }
         
     }
     catch (Exception e){
-
         Console.WriteLine("Error");
     }
+}
+
+if (args[0] == "cheep")
+{
+    string message = args[1];
+   // log time
+   //log logged in user
+   // combine to one line seperator with " " and ,
+   // write to csv
 }

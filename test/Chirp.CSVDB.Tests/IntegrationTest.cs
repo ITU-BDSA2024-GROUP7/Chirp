b7b2195d68167c93d905f1acebe5f5483159ca86
Nikolaj <nals@itu.dk>
2024-09-11 21:10:03 +0200

    All current integration tests pass, and use relative path to data directory
    All integration tests now uses relative path instead of absolute path.
    
    CSVData "TestWriteReadRecord" integration test now passes as we have fixed syntax errors causing TestWriteReadRecord() to throw cannot convert exception.
    
    Co-Authored-By: Rasmus Alexander Christiansen <54313567+RasmusAChr@users.noreply.github.com>

--- test/Chirp.CSVDB.Tests/IntegrationTest.cs
+++ test/Chirp.CSVDB.Tests/IntegrationTest.cs
@@ -1,55 +1,67 @@
 public class DatabaseTests
 {
-    public record Info(string author, string message, long timeStamp);
+    public record DataRecord(string Author, string Message, long Timestamp);
 
     [Theory]
     [InlineData("John Doe", "Hello World!", 1645480000)]
     [InlineData("Jane Doe", "Goodbye World!", 1645480001)]
-    public void TestWriteReadRecord(string author, string message, long timeStamp)
+    public void TestWriteReadRecord(string authorData, string messageData, long timestampData)
     {
         // Arrange
-        var db = new CSVDatabase<Cheep>("C:/Users/nik/OneDrive/Documents/GitHub/Chirp/data/chirp_cli_db.csv");
-        var testRecord = new Info(author, message, timeStamp);
-        // record testRecord = new record(author, message, timeStamp);
+        var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        var dbPath = Path.Combine(baseDirectory, "../../../../../data/chirp_cli_db.csv");
+        var db = new CSVDatabase<DataRecord>(dbPath);
+        var testRecord = new DataRecord(authorData, messageData, timestampData);
 
         // Act
         db.Store(testRecord);
-        Cheep result = db.Read(1).Last();
+        var result = db.Read(1).Last();
 
         // Assert
-        Assert.Equal(author, result.Author);
-        Assert.Equal(message, result.Message);
-        Assert.Equal(timeStamp, result.Timestamp);
+        Assert.Equal(authorData, result.Author);
+        Assert.Equal(messageData, result.Message);
+        Assert.Equal(timestampData, result.Timestamp);
     }
 
     [Fact]
     public void TestReadWriteMultipleRecords()
     {
         // Arrange
-        var db = new CSVDatabase<Cheep>("C:/Users/nik/OneDrive/Documents/GitHub/Chirp/data/chirp_cli_db.csv");
-        string message = "Hello World!";
-        string author = "John Doe";
+        var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        var dbPath = Path.Combine(baseDirectory, "../../../../../data/chirp_cli_db.csv");
+        var db = new CSVDatabase<Cheep>(dbPath);
         long timeStamp = DateTimeOffset.Now.ToUnixTimeSeconds();
-        var record = new Cheep(author, message, timeStamp);
+        var record1 = new Cheep("John Doe", "Hello World!", timeStamp);
+        var record2 = new Cheep("Jane Doe", "Goodbye World!", timeStamp);
 
         // Act
-        //db.Store(cheep);
-        db.Store(record);
-        Cheep result = db.Read(1).Last();
-        // Console.log("outCheep: ", outCheep);
+        db.Store(record1);
+        db.Store(record2);
+        // Cheep result = db.Read(1).Last();
+        List<Cheep> resultList = db.Read().ToList();
+
+        // Get last and second last records
+        Cheep result1 = resultList[resultList.Count - 2]; // John Doe
+        Cheep result2 = resultList[resultList.Count - 1]; // Jane Doe
 
         // Assert
-        // format is: adho @ 08/02/23 14:37:38: I hope you had a good summer.
-        Assert.Equal(author, result.Author);
-        Assert.Equal(message, result.Message);
-        Assert.Equal(timeStamp, result.Timestamp);
+        // John Doe
+        Assert.Equal("John Doe", result1.Author);
+        Assert.Equal("Hello World!", result1.Message);
+        Assert.Equal(timeStamp, result1.Timestamp);
+        // Jane Doe
+        Assert.Equal("Jane Doe", result2.Author);
+        Assert.Equal("Goodbye World!", result2.Message);
+        Assert.Equal(timeStamp, result2.Timestamp);
     }
 
     [Fact]
     public void TestReadWriteCheep()
     {
         // Arrange
-        var db = new CSVDatabase<Cheep>("C:/Users/nik/OneDrive/Documents/GitHub/Chirp/data/chirp_cli_db.csv");
+        var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        var dbPath = Path.Combine(baseDirectory, "../../../../../data/chirp_cli_db.csv");
+        var db = new CSVDatabase<Cheep>(dbPath);
         string author = "MelonMusk";
         string message = "Wait this is not X the everuthing app";
         long timeStamp = DateTimeOffset.Now.ToUnixTimeSeconds();

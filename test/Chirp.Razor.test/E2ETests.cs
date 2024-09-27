978c771f0b7bc19349c99a94942d77bf4cb8454a
Nikolaj Carlson <nhca@itu.dk>
2024-09-27 16:59:16 +0200

    Added 2 E2E tests for Razor Pages

---
+++ test/Chirp.Razor.test/E2ETests.cs
@@ -0,0 +1,43 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc.Testing;
+using FluentAssertions;
+using Chirp.Razor;
+
+namespace Chirp.Razor.test;
+
+public class E2ETests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+
+    public E2ETests(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact]
+    public async Task GetIndexPageAndCorrectContent()
+    {
+        var client = _factory.CreateClient();
+        
+        var response = await client.GetAsync("/");
+        
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        var content = await response.Content.ReadAsStringAsync();
+        content.Should().Contain("Chirp!");
+    }
+
+    [Fact]
+    public async Task Get_TestPersonPage()
+    {
+        var client = _factory.CreateClient();
+    
+        var response = await client.GetAsync("/Testperson?page=1");
+    
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        var content = await response.Content.ReadAsStringAsync();
+    
+        // Check for the actual content on the page
+        content.Should().Contain("There are no cheeps so far.");
+    }
+
+}

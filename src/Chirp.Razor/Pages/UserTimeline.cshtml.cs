fbc455ba368f2db2ab2c5cccefccfbf147bd82d2
RasmusAC <ralc@itu.dk>
2024-09-27 17:45:11 +0200

    Refactored pageNumber
    The changes are the same for Public timeline and User timeline.
    
    Initially we checked the pageNumber for several conditions inside the .cshtml file. Now it gets handled inside the OnGet function in the .cshtml.cs file.
    
    I have created a get/set variable for the pageNumber inside the .cshtml.cs file. This variable can be accessed inside the .cshtml file. This means we only have to handle errors (like wrong datatype, valid page etc) inside the .cshtml.cs file.

--- src/Chirp.Razor/Pages/UserTimeline.cshtml.cs
+++ src/Chirp.Razor/Pages/UserTimeline.cshtml.cs
@@ -6,6 +6,7 @@ namespace Chirp.Razor.Pages;
 public class UserTimelineModel : PageModel
 {
     private readonly ICheepService _service;
+    public int pageNumber { get; set; }
     public List<CheepViewModel> Cheeps { get; set; }
 
     public UserTimelineModel(ICheepService service)
@@ -16,6 +17,12 @@ public class UserTimelineModel : PageModel
     // Runs when site is loaded (Request Method:GET)
     public ActionResult OnGet(string author, [FromQuery] int page)
     {
+        if (!(page is int) || page <= 0)
+        {
+            page = 1;
+        }
+        
+        pageNumber = page;
         Cheeps = _service.GetCheepsFromAuthor(author, page);
         return Page();
     }

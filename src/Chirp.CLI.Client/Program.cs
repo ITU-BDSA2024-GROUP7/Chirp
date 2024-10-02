6a53d482d141ee614049a33cd06598a32248ec81
Mathias Larsen <mnla@itu.dk>
2024-10-02 20:09:24 +0200

    Updated location of the azure

--- src/Chirp.CLI.Client/Program.cs
+++ src/Chirp.CLI.Client/Program.cs
@@ -12,7 +12,7 @@ Usage:
 
 
 var arguments = new Docopt().Apply(usage, args, version: "1.0", exit: true)!;
-const string baseUrl = "bdsagroup07chirpremotedb.azurewebsites.net";
+const string baseUrl = "https://bdsagroup07chirpremotedb.azurewebsites.net/";
 using HttpClient client = new();
 client.BaseAddress = new Uri(baseUrl);
 
@@ -62,7 +62,7 @@ async Task PostCheep(IDictionary<string, ValueObject> arguments, HttpClient clie
         TimeZoneInfo cetZone = TimeZoneInfo.FindSystemTimeZoneById("Central European Standard Time");
         DateTimeOffset cetTime = TimeZoneInfo.ConvertTime(DateTimeOffset.Now, cetZone);
         long date = cetTime.ToUnixTimeSeconds();
-
+        
         await client.PostAsJsonAsync("/cheep", new Cheep(author, message, date));
         
     // database.Store(new Cheep(author, message, date));

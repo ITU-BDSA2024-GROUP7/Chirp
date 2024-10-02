076b5a9341718f1c26d9c5f38f757e0a99c2889a
Nikolaj <nals@itu.dk>
2024-10-02 15:00:02 +0200

    Update CheepRepository.cs
    Co-Authored-By: Rasmus Alexander Christiansen <54313567+RasmusAChr@users.noreply.github.com>
    Co-Authored-By: Tipskind <151034101+Tipskind@users.noreply.github.com>
    Co-Authored-By: Nikolaj Carlson <70134368+Nikolaj787@users.noreply.github.com>
    Co-Authored-By: Mathias Niklas Larsen <143803020+DragonFlyersx@users.noreply.github.com>

--- src/Chirp.Razor/CheepRepository.cs
+++ src/Chirp.Razor/CheepRepository.cs
@@ -29,7 +29,17 @@ public class CheepRepository : ICheepRepository
     
     public async Task CreateMessage(CheepDTO cheep)
     {
-        Cheep newMessage = new() { Text = cheep.Text, Author = cheep.Author, TimeStamp = cheep.Timestamp };
+        // Define the query - with our setup, EF Core translates this to an SQLite query in the background
+        var query = from cheep in _dbContext.Cheeps
+            where cheep.Author.Name == "Adrian"
+            select new { cheep.Text, cheep.Author };
+            // Execute the query and store the results
+        var result = await query.ToListAsync();
+        
+        Cheep newCheep = new() { Text = cheep.Text, Author = cheep.AuthorId, TimeStamp = long.Parse(cheep.TimeStamp) };
+        
+        // Messages Text , Author som skal reference til, System date time to long
+        
         var queryResult = await _dbContext.cheep.AddAsync(newMessage); // does not write to the database!
 
         await _dbContext.SaveChangesAsync(); // persist the changes in the database
@@ -39,7 +49,7 @@ public class CheepRepository : ICheepRepository
 
 public class CheepDTO
 {
-    public String Text { get; set; }
-    public String Author { get; set; }
+    public string Text { get; set; }
+    public int AuthorId { get; set; }
     public long TimeStamp { get; set; }
 }

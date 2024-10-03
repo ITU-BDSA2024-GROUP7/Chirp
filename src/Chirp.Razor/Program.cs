bd012948e5ffb5c1c05e88a6ae1c23aec0d29b31
Nikolaj <nals@itu.dk>
2024-10-03 14:33:23 +0200

    Logging and attempt at fix
    added more verbose data logging
    Update appsettings.json
    added include migration folder in .csproj
    Logging for migration files

--- src/Chirp.Razor/Program.cs
+++ src/Chirp.Razor/Program.cs
@@ -14,14 +14,24 @@ namespace Chirp.Razor
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container
-            builder.Services.AddRazorPages(); 
+            builder.Services.AddRazorPages();
             builder.Services.AddScoped<CheepRepository>();
-           // builder.Services.AddScoped<ICheepRepository, CheepRepository>();
-            
+            // builder.Services.AddScoped<ICheepRepository, CheepRepository>();
+
 
             // Load database connection via configuration
             string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-            builder.Services.AddDbContext<CheepDBContext>(options => options.UseSqlite(connectionString));
+            builder.Services.AddDbContext<CheepDBContext>(options => options.UseSqlite(connectionString).EnableSensitiveDataLogging()); // Disable sensitive data logging when issue is fixed
+
+            var logger = builder.Services.BuildServiceProvider().GetRequiredService<ILogger<Program>>();
+            if (string.IsNullOrWhiteSpace(connectionString))
+            {
+                logger.LogError("Connection string is null or empty.");
+            }
+            else
+            {
+                logger.LogInformation($"Using connection string: {connectionString}");
+            }
 
             // Build the application
             var app = builder.Build();
@@ -31,10 +41,10 @@ namespace Chirp.Razor
             {
                 using var migrationContext = scope.ServiceProvider.GetService<CheepDBContext>();
                 migrationContext.Database.Migrate();
-                
+
                 var services = scope.ServiceProvider;
                 var context = services.GetRequiredService<CheepDBContext>();
-                
+
                 // Call the SeedDatabase method
                 DbInitializer.SeedDatabase(context);
             }

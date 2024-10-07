94fb9c7d94f26a10ef68b5c0d6f6ef0f8818ce85
Nikolaj Carlson <nhca@itu.dk>
2024-10-07 15:32:00 +0200

    Removed logging
    Removed logging since issues were fixed, so they are unnecessary now

--- src/Chirp.Razor/Program.cs
+++ src/Chirp.Razor/Program.cs
@@ -21,18 +21,7 @@ namespace Chirp.Razor
 
             // Load database connection via configuration
             string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-            builder.Services.AddDbContext<CheepDBContext>(options => options.UseSqlite(connectionString).EnableSensitiveDataLogging()); // Disable sensitive data logging when issue is fixed
-
-            var logger = builder.Services.BuildServiceProvider().GetRequiredService<ILogger<Program>>();
-            if (string.IsNullOrWhiteSpace(connectionString))
-            {
-                logger.LogError("Connection string is null or empty.");
-            }
-            else
-            {
-                logger.LogInformation($"Using connection string: {connectionString}");
-            }
-            logger.LogInformation($"Connection string from environment: '{Environment.GetEnvironmentVariable("DefaultConnection")}'");
+            builder.Services.AddDbContext<CheepDBContext>(options => options.UseSqlite(connectionString)); 
 
             // Build the application
             var app = builder.Build();
@@ -40,26 +29,11 @@ namespace Chirp.Razor
             // Seed the database after the application is built
             using (var scope = app.Services.CreateScope())
             {
-                // old code, outcommented for testing
-                /*
-                using var migrationContext = scope.ServiceProvider.GetService<CheepDBContext>();
-                migrationContext.Database.Migrate();
-                */
-                
                 var services = scope.ServiceProvider;
                 var context = services.GetRequiredService<CheepDBContext>();
                 
-                
-                try
-                {
-                    context.Database.Migrate();
-                    DbInitializer.SeedDatabase(context);
-                    logger.LogInformation("Database seeded successfully.");
-                }
-                catch (Exception ex)
-                {
-                    logger.LogError(ex, "An error occurred while seeding the database.");
-                }
+                context.Database.Migrate();
+                DbInitializer.SeedDatabase(context);
             }
 
             // Configure the HTTP request pipeline

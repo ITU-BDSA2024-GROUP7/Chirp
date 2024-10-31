fff9566e61f0c7e36484be04f7ccfe8a0f34323c
RasmusAC <ralc@itu.dk>
2024-10-31 17:32:34 +0100

    Added foreign key for AuthorId

--- src/Chirp.Core/Cheep.cs
+++ src/Chirp.Core/Cheep.cs
@@ -8,6 +8,7 @@ public class Cheep
     [Key]
     public int CheepId { get; set; } // Primary Key
     public required Author Author { get; set; } // Navigation Property
+    [ForeignKey("AuthorId")]
     public int AuthorId { get; set; }
     
     // Making sure that the total length of the message is less than 160 characters

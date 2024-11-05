ccc098803f42446098e24898bab952c06813010a
Nikolaj <nals@itu.dk>
2024-11-05 20:16:30 +0100

    Update UserTimeline.cshtml.cs
    Co-Authored-By: Rasmus Alexander Christiansen <54313567+RasmusAChr@users.noreply.github.com>

--- src/Chirp.Web/Pages/UserTimeline.cshtml.cs
+++ src/Chirp.Web/Pages/UserTimeline.cshtml.cs
@@ -15,7 +15,7 @@ public class UserTimelineModel : PageModel
     public int TotalPageNumber { get; set; }
     public SharedChirpViewModel SharedViewModel { get; set; } = new SharedChirpViewModel();
     public required List<CheepDTO> Cheeps { get; set; }
-
+    public string CurrentAuthor;
     public UserTimelineModel(CheepService service)
     {
         _service = service;
@@ -28,7 +28,8 @@ public class UserTimelineModel : PageModel
         {
             page = 1;
         }
-        
+
+        CurrentAuthor = author;
         PageNumber = page;
         Cheeps = await _service.GetCheepsFromAuthor(author, page);
         TotalPageNumber = await _service.GetTotalPageNumber(author);
@@ -45,7 +46,7 @@ public class UserTimelineModel : PageModel
         if (!ModelState.IsValid) // Check if the model state is invalid
         {
             // Ensure Cheeps and other required properties are populated
-            Cheeps = await _service.GetCheeps(PageNumber);
+            Cheeps = await _service.GetCheepsFromAuthor(CurrentAuthor, PageNumber);
             TotalPageNumber = await _service.GetTotalPageNumber();
             return Page(); // Return the page with validation messages
         }

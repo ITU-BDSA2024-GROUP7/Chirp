d95bec76eef5730f447a82f42c2745d7219cb8eb
Mathias Niklas Larsen <mnla@itu.dk>
2024-11-13 16:43:58 +0100

    Update ExternalLogin.cshtml.cs
    Removed unnesasarry confirmation from github oauth
    
    Co-Authored-By: Rasmus Alexander Christiansen <54313567+RasmusAChr@users.noreply.github.com>

--- src/Chirp.Web/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
+++ src/Chirp.Web/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
@@ -145,27 +145,8 @@ namespace Chirp.Web.Areas.Identity.Pages.Account
                     result = await _userManager.AddLoginAsync(user, info);
                     if (result.Succeeded)
                     {
-                        _logger.LogInformation("User created an account using {Name} provider.", info.LoginProvider);
-
-                        var userId = await _userManager.GetUserIdAsync(user);
-                        var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                        code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                        var callbackUrl = Url.Page(
-                            "/Account/ConfirmEmail",
-                            pageHandler: null,
-                            values: new { area = "Identity", userId = userId, code = code },
-                            protocol: Request.Scheme);
-
-                        await _emailSender.SendEmailAsync(Email, "Confirm your email",
-                            $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-
-                        // If account confirmation is required, we need to show the link if we don't have a real email sender
-                        if (_userManager.Options.SignIn.RequireConfirmedAccount)
-                        {
-                            return RedirectToPage("./RegisterConfirmation", new { Email = Email });
-                        }
-
                         await _signInManager.SignInAsync(user, isPersistent: false, info.LoginProvider);
+                        Console.WriteLine("Signed in user");
                         return LocalRedirect(returnUrl);
                     }
                 }

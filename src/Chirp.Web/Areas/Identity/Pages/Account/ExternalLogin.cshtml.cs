fad5e9268d49c3a1ae66f9b4a76ea399885e4583
RasmusAC <ralc@itu.dk>
2024-11-13 20:30:45 +0100

    Comments and cshtml changes for externallogin
    Externallogin.cshtml.cs:
    - Added comments
    - Removed console logs
    
    .cshtml:
    - Removed not used cshtml.

--- src/Chirp.Web/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
+++ src/Chirp.Web/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
@@ -2,22 +2,13 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using System;
-using System.ComponentModel.DataAnnotations;
 using System.Security.Claims;
-using System.Text;
-using System.Text.Encodings.Web;
-using System.Threading;
-using System.Threading.Tasks;
 using Chirp.Infrastructure.Data;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.Extensions.Options;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.WebUtilities;
-using Microsoft.Extensions.Logging;
 
 namespace Chirp.Web.Areas.Identity.Pages.Account
 {
@@ -28,7 +19,6 @@ namespace Chirp.Web.Areas.Identity.Pages.Account
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly IUserStore<ApplicationUser> _userStore;
         private readonly IUserEmailStore<ApplicationUser> _emailStore;
-        private readonly IEmailSender _emailSender;
         private readonly ILogger<ExternalLoginModel> _logger;
 
         public ExternalLoginModel(
@@ -43,108 +33,95 @@ namespace Chirp.Web.Areas.Identity.Pages.Account
             _userStore = userStore;
             _emailStore = GetEmailStore();
             _logger = logger;
-            _emailSender = emailSender;
         }
-
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        public string ProviderDisplayName { get; set; }
-
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
+        
         public string ReturnUrl { get; set; }
-
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
+        
         [TempData]
         public string ErrorMessage { get; set; }
-
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        public IActionResult OnGet() => RedirectToPage("./Login"); // Redirect user to login page
-
+        
+        // Users should not be able to access this page directly. If so, redirect to login page.
+        public IActionResult OnGet() => RedirectToPage("./Login");
+        
+        // When user clicks on a 'login using (an oauth application) button', this method is called.
         public IActionResult OnPost(string provider, string returnUrl = null)
         {
-            Console.WriteLine("onpost");
             // Request a redirect to the external login provider.
             var redirectUrl = Url.Page("./ExternalLogin", pageHandler: "Callback", values: new { returnUrl });
             var properties = _signInManager.ConfigureExternalAuthenticationProperties(provider, redirectUrl);
             return new ChallengeResult(provider, properties);
         }
 
+        // This method is called when the external login provider has authenticated the user.
         public async Task<IActionResult> OnGetCallbackAsync(string returnUrl = null, string remoteError = null)
         {
-            Console.WriteLine("ongetcallback");
             returnUrl = returnUrl ?? Url.Content("~/");
+            
+            // If there is an error from the external provider, display it.
             if (remoteError != null)
             {
                 ErrorMessage = $"Error from external provider: {remoteError}";
                 return RedirectToPage("./Login", new { ReturnUrl = returnUrl });
             }
+            
+            // Get the information about the user from the external login provider.
             var info = await _signInManager.GetExternalLoginInfoAsync();
-            ProviderDisplayName = info?.ProviderDisplayName;
+            
+            // If there is no external login information, redirect to login page.
             if (info == null)
             {
                 ErrorMessage = "Error loading external login information.";
                 return RedirectToPage("./Login", new { ReturnUrl = returnUrl });
             }
 
-            // Sign in the user with this external login provider if the user already has a login.
+            // Signs in the user with the external login provider, if the user already has a login.
             var result = await _signInManager.ExternalLoginSignInAsync(info.LoginProvider, info.ProviderKey, isPersistent: false, bypassTwoFactor: true);
-            if (result.Succeeded)
+            
+            if (result.Succeeded) // If the user is successfully signed in.
             {
-                _logger.LogInformation("{Name} logged in with {LoginProvider} provider.", info.Principal.Identity.Name, info.LoginProvider);
+                _logger.LogInformation("{Name} logged in with {LoginProvider} provider.", info.Principal.Identity?.Name, info.LoginProvider);
                 return LocalRedirect(returnUrl);
             }
-            if (result.IsLockedOut)
+            if (result.IsLockedOut) // If the user is locked out
             {
                 return RedirectToPage("./Lockout");
             }
-            else
+            
+            // If the user does not have an account, then create an account.
+            if (ModelState.IsValid) // If the model state is valid (i.e. the user has provided all the required information)
             {
-                if (ModelState.IsValid)
-                {
-                    var user = CreateUser();
+                var user = CreateUser();
 
-                    var email = info.Principal.FindFirstValue(ClaimTypes.Email);
-                    var name = info.Principal.FindFirstValue(ClaimTypes.Name);
-                    Console.WriteLine("Email: " + email);
-                    Console.WriteLine("Name: " + name);
+                var email = info.Principal.FindFirstValue(ClaimTypes.Email);
+                var name = info.Principal.FindFirstValue(ClaimTypes.Name);
+            
+                await _userStore.SetUserNameAsync(user, name, CancellationToken.None);
+                await _emailStore.SetEmailAsync(user, email, CancellationToken.None);
                 
-                    await _userStore.SetUserNameAsync(user, name, CancellationToken.None);
-                    await _emailStore.SetEmailAsync(user, email, CancellationToken.None);
-                    user.EmailConfirmed = true;
+                user.EmailConfirmed = true;
 
-                    var accountResult = await _userManager.CreateAsync(user);
-                    Console.WriteLine("result: " + accountResult);
-                    if (accountResult.Succeeded)
+                var accountResult = await _userManager.CreateAsync(user); // Create the user account and save the result.
+                
+                if (accountResult.Succeeded) // If the account is successfully created.
+                {
+                    accountResult = await _userManager.AddLoginAsync(user, info); // Add the external login to the user account.
+                    if (accountResult.Succeeded) // If the external login is successfully added to the user account.
                     {
-                        accountResult = await _userManager.AddLoginAsync(user, info);
-                        if (accountResult.Succeeded)
-                        {
-                            await _signInManager.SignInAsync(user, isPersistent: false, info.LoginProvider);
-                            Console.WriteLine("Signed in user");
-                            return LocalRedirect(returnUrl);
-                        }
-                    }
-                    foreach (var error in accountResult.Errors)
-                    {
-                        ModelState.AddModelError(string.Empty, error.Description);
+                        await _signInManager.SignInAsync(user, isPersistent: false, info.LoginProvider); // Sign in the user.
+                        return LocalRedirect(returnUrl); // Redirect to the return URL.
                     }
                 }
-
-                ProviderDisplayName = info.ProviderDisplayName;
-                ReturnUrl = returnUrl;
-                return Page();
+                
+                // If the account creation or adding the external login to the account fails, then add the errors to the model state.
+                foreach (var error in accountResult.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
             }
+
+            // If the model state is not valid, then redisplay the page.
+            ReturnUrl = returnUrl;
+            return Page();
         }
 
         private ApplicationUser CreateUser()

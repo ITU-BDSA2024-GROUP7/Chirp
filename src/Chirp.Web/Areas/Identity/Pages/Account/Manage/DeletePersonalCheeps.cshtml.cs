f3b04b12f38ea749d0550356b20fc8858d8bde2d
RasmusAC <ralc@itu.dk>
2024-11-20 18:00:49 +0100

    Added a option to just delete cheeps

---
+++ src/Chirp.Web/Areas/Identity/Pages/Account/Manage/DeletePersonalCheeps.cshtml.cs
@@ -0,0 +1,113 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+#nullable disable
+
+using System.ComponentModel.DataAnnotations;
+using Chirp.Core.DTOs;
+using Chirp.Infrastructure.Data;
+using Chirp.Infrastructure.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Chirp.Web.Areas.Identity.Pages.Account.Manage
+{
+    public class DeletePersonalCheepsModel : PageModel
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ILogger<DeletePersonalDataModel> _logger;
+        private readonly CheepService _service;
+        
+
+        public DeletePersonalCheepsModel(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            ILogger<DeletePersonalDataModel> logger,
+            CheepService service)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+            _service = service;
+        }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public class InputModel
+        {
+            /// <summary>
+            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+            ///     directly from your code. This API may change or be removed in future releases.
+            /// </summary>
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+            public string Username { get; set; }
+        }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public bool RequirePassword { get; set; }
+
+        public async Task<IActionResult> OnGet()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            RequirePassword = await _userManager.HasPasswordAsync(user);
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+            
+            
+            if (await _userManager.GetUserNameAsync(user) != Input.Username)
+            {
+                ModelState.AddModelError(string.Empty, "Incorrect username.");
+                return Page();
+            }
+
+            if (User.Identity != null && User.Identity.IsAuthenticated)
+            {
+                var authorName = User.Identity.Name;
+                var authorEmail = User.Identity.Name;
+
+                if (authorName != null)
+                {
+                    if (authorEmail != null)
+                    {
+                        var author = new AuthorDTO
+                        {
+                            Name = authorName,
+                            Email = authorEmail
+                        };
+                        await _service.DeleteUserCheeps(author);
+                    }
+                }
+            }
+
+            return Page();
+        }
+    }
+}

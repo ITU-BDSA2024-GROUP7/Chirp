0525eef788d0440db0d7f30ff4ee5e8b59428048
Nikolaj Alsøe <nals@itu.dk>
2024-12-04 11:10:22 +0100

    Supports different page lengths but not widths

---
+++ src/Chirp.Web/wwwroot/js/background-gradient.js
@@ -0,0 +1,25 @@
+﻿window.addEventListener('load', function() {
+    ResizeBackground();
+});
+
+window.addEventListener("resize", function() {
+    ResizeBackground();
+});
+
+function ResizeBackground(){
+    const dayImage = new Image();
+    dayImage.src = '../images/Day.png';
+    dayImage.onload = function() {
+        const bodyHeight = document.body.clientHeight;
+        const documentHeight = document.documentElement.clientHeight;
+        const dayImageHeight = dayImage.height;
+        const remainingHeight = bodyHeight - dayImageHeight;
+        const dayImageHeightPercentage = (remainingHeight / bodyHeight) * 100;
+        if (dayImageHeightPercentage < 0) {
+            document.documentElement.style.setProperty('--day-gradient-height', `0%`);
+            return;
+        } else {
+            document.documentElement.style.setProperty('--day-gradient-height', `${dayImageHeightPercentage}%`);
+        }
+    };
+}

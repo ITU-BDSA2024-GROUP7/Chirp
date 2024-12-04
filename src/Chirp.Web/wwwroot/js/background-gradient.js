0f6b16bdc8a216fe1f74ac7c5d045206cec33ccd
Nikolaj Alsøe <nals@itu.dk>
2024-12-04 11:49:22 +0100

    Now supports different widths
    It now uses the actual size of the background image in the viewport to calculate the aspect ratio and properly calculate the size of y-axis for the gradient.

--- src/Chirp.Web/wwwroot/js/background-gradient.js
+++ src/Chirp.Web/wwwroot/js/background-gradient.js
@@ -6,12 +6,22 @@ window.addEventListener("resize", function() {
     ResizeBackground();
 });
 
+
+// Calculation inspired by https://stackoverflow.com/questions/76476276/how-to-get-the-aspect-ratio-of-an-image-using-javascript
+// and https://stackoverflow.com/questions/74472466/how-to-get-the-actual-dimensions-of-the-element-displayed-in-viewport
 function ResizeBackground(){
     const dayImage = new Image();
     dayImage.src = '../images/Day.png';
     dayImage.onload = function() {
+        document.body.appendChild(dayImage); // Temporarily add the image to the DOM to get its rendered size
+        const actualDayImageHeight = dayImage.getBoundingClientRect().height;
+        const actualDayImageWidth = dayImage.getBoundingClientRect().width;
+        document.body.removeChild(dayImage); // Remove the image after getting its size
+
         const bodyHeight = document.body.clientHeight;
-        const dayImageHeight = dayImage.height;
+        const bodyWidth = document.body.clientWidth;
+        const aspectRatio = actualDayImageWidth / actualDayImageHeight;
+        const dayImageHeight = bodyWidth / aspectRatio;
         const remainingHeight = bodyHeight - dayImageHeight;
         const dayImageHeightPercentage = (remainingHeight / bodyHeight) * 100;
         if (dayImageHeightPercentage < 0) {
